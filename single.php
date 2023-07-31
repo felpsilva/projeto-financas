f8ca2ef48e78c3ceb3118ca786a48d7e1ec90a23
felpsilva <felipep.silva5741@hotmail.com>
2023-07-31 01:15:38 -0300

    inserindo o nome do autor e a data de publicação nos posts

--- single.php
+++ single.php
@@ -5,6 +5,7 @@ $img_title = get_post_meta($img_id, '_wp_attachment_image_alt', true)
 ?>
 
 <body>
+<?php while (have_posts()) : the_post(); ?>
     <header>
         <div>
             <div>
@@ -28,16 +29,18 @@ $img_title = get_post_meta($img_id, '_wp_attachment_image_alt', true)
                 <p class="resumo">
                     <?php the_excerpt() ?>
                 </p>
+                <p class="autor">Por: <?php the_author(); ?></p>
+                <p class="data">Data de publicação: <?php the_time('d/m/Y - H:i'); ?></p>    
                 <div class="imgPost"><img src="<?php the_post_thumbnail_url()?>" alt="<?= $img_title?>"></div>
                 <div class="ads-horizontal"></div>
                 <?php the_content() ?>
             </article>
         </section>
     </main>
+    <?php endwhile; ?>
     <footer>
         <h2>Contatos</h2>
     </footer>
-
 </body>
 
 </html>
